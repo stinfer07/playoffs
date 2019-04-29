2da130e1728fbbe5bdada2fa6ada5b4ff51d3ea4
ivanromero00 <44954580+ivanromero00@users.noreply.github.com>
2019-04-29 10:47:20 +0200

    Add files via upload
    29/04/2019 Ivan Romero/Fran Muñoz

--- playoff_clases.py
+++ playoff_clases.py
@@ -1,33 +1,43 @@
+from datetime import timedelta
+
 class Equipo:
-    def __init__(self, nombre, iniciales):
+    def __init__(self, nombre, iniciales, logo = ''):
         self.nombre = nombre
         self.iniciales = iniciales
+        self.logo = logo
 
     def __str__(self):
         return self.nombre + '(' + self.iniciales + ')'
 
 class Partido:
-    def __init__(self, ganador, fecha, equipos = []):
+    def __init__(self, fecha, equipos = [], ganador = None):
         self.equipos = equipos
-        self.ganador = ganador
         self.fecha = fecha
+        self.ganador = ganador
     
     def __str__(self):
-        return self.equipos[0] + ' vs ' + self.equipos[1] + '\nGanador: ' + self.ganador
+        if self.ganador == None:
+            return str(self.equipos[0]) + ' vs ' + str(self.equipos[1])
+        else:
+            return str(self.equipos[0]) + ' vs ' + str(self.equipos[1]) + '\tGanador: ' + str(self.ganador)
 
 class Eliminatoria:
-    def __init__(self, ganador, equipos = [], partidos = []):
+    def __init__(self, equipos = [], partidos = [], ganador = None):
         self.partidos = partidos
         self.equipos = equipos
         self.ganador = ganador
 
     def __str__(self):
-        return 'La eliminatoria se han jugado ' + str(len(self.resultado)) + '\nGanador: ' + self.ganador
+        if self.ganador == None:
+            return 'Eliminatoria: ' + str(self.equipos[0]) + ' vs ' + str(self.equipos[1])
+        else:
+            return 'Eliminatoria: ' + str(self.equipos[0]) + ' vs ' + str(self.equipos[1]) + '\tGanador: ' + str(self.ganador)
 
-    #def nuevo_partido(self):
+    def nuevo_partido(self, equipo1, equipo2, fecha):
+        Partido(fecha, [equipo1, equipo2])
 
 class Temporada:
-    def __init__(self, nombre, equipos = [], eliminatoias = []):
+    def __init__(self, nombre, equipos = [], eliminatorias = []):
         self.nombre = nombre
         self.equipos = equipos
         self.eliminatorias = eliminatorias
@@ -38,4 +48,9 @@ class Temporada:
             string += '\t' + str(equipo) + '\n'
         return string
     
-    #def nueva_eliminatoria(self, ):
+    def nueva_eliminatoria(self, equipo1, equipo2, fecha):
+        lista_partidos = []
+        for i in range(4):
+            lista_partidos.append(Partido(fecha, [equipo1, equipo2]))
+            fecha += timedelta(days=2)
+        self.eliminatorias.append(Eliminatoria([equipo1, equipo2], lista_partidos))
