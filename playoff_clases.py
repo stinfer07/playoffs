4ee24c97f0c2b706cbc726ae00c02cfbdd0034e4
Iván Romero Reyna <ivanromeroreyna@pc163-10.11001828.11.andared.ced.junta-andalucia.es>
2019-04-29 09:24:33 +0200

    29/04/2019 - Ivan Romero

--- playoff_clases.py
+++ playoff_clases.py
@@ -1,40 +1,41 @@
 class Equipo:
-    def __init__(self, nombre, iniciales, partidos_ganados = 0):
+    def __init__(self, nombre, iniciales):
         self.nombre = nombre
         self.iniciales = iniciales
-        self.partidos_ganados = partidos_ganados
 
     def __str__(self):
         return self.nombre + '(' + self.iniciales + ')'
 
 class Partido:
-    def __init__(self, ganador, equipos = []):
+    def __init__(self, ganador, fecha, equipos = []):
         self.equipos = equipos
         self.ganador = ganador
+        self.fecha = fecha
     
     def __str__(self):
         return self.equipos[0] + ' vs ' + self.equipos[1] + '\nGanador: ' + self.ganador
 
 class Eliminatoria:
-    def __init__(self, ganador, equipos = [], resultado = []):
-        self.resultado = resultado
+    def __init__(self, ganador, equipos = [], partidos = []):
+        self.partidos = partidos
         self.equipos = equipos
         self.ganador = ganador
 
     def __str__(self):
         return 'La eliminatoria se han jugado ' + str(len(self.resultado)) + '\nGanador: ' + self.ganador
 
+    #def nuevo_partido(self):
+
 class Temporada:
-    def __init__(self, nombre, equipos = [], resultados = []):
+    def __init__(self, nombre, equipos = [], eliminatoias = []):
         self.nombre = nombre
         self.equipos = equipos
-        self.resultados = resultados
+        self.eliminatorias = eliminatorias
 
     def __str__(self):
         string = 'Temporada: ' + self.nombre + '\n'
         for equipo in self.equipos:
             string += '\t' + str(equipo) + '\n'
         return string
-        
-    #def nuevo_resultado()):
-    # Falta definir la introduccion de un nuevo resultado.
+    
+    #def nueva_eliminatoria(self, ):
